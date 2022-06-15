a4d99c99975b4ef699548b2e0dc3fac60c0f64d3
Michael McDonnell <Michael.McDonnell067@gmail.com>
2022-06-15 15:05:30 -0500

    imported word.js to board.js to use the matrix

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {boardDefault} from '../Words'
 
 function Board() {
+  const [board, setBoard] = useState(boardDefault);
   return (
     <div>Board</div>
   )
