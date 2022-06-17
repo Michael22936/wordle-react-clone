ad437c35001df51a7cfdc3609ea4017e1afe0b07
Michael McDonnell <Michael.McDonnell067@gmail.com>
2022-06-16 20:28:45 -0500

    created the keyboard divs

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Keyboard() {
   return (
-    <div className='keyboard'>Keyboard</div>
+    <div className="keyboard">
+      <div className="line1"></div>
+      <div className="line2"></div>
+      <div className="line3"></div>
+    </div>
   )
 }
 
