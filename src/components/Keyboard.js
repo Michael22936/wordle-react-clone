2af692000fd69faaf00b92667c09e0bce8e8c0ec
Michael McDonnell <Michael.McDonnell067@gmail.com>
2022-06-16 20:38:16 -0500

    did the maping for the remaining two arrays of keys

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -9,11 +9,21 @@ function Keyboard() {
 
   return (
     <div className="keyboard">
-      <div className="line1">{keys1.map((key) => {
-        return <div> {key} </div>
-      })}</div>
-      <div className="line2"></div>
-      <div className="line3"></div>
+      <div className="line1">
+        {keys1.map((key) => {
+          return <div> {key} </div>
+        })}
+      </div>
+      <div className="line2">
+      {keys2.map((key) => {
+          return <div> {key} </div>
+        })}
+      </div>
+      <div className="line3">
+      {keys3.map((key) => {
+          return <div> {key} </div>
+        })}
+      </div>
     </div>
   )
 }
