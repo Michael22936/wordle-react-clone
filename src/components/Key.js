f511aa55e39d463c98c00d0380c89423ebf6a727
Michael McDonnell <Michael.McDonnell067@gmail.com>
2022-06-20 15:54:28 -0500

    finished the enter key functionality

--- src/components/Key.js
+++ src/components/Key.js
@@ -9,7 +9,8 @@ function Key({ keyVal, bigKey }) {
 
   const selectLetter = () => {
     if (keyVal === "ENTER"){
-
+        if(currAttempt.letterPos !== 5) return;
+        setCurrAttempt({attempt: currAttempt.attempt + 1, letterPos: 0})
     } else {
       if (currAttempt.letterPos > 4) return;
     const newBoard = [...board]
