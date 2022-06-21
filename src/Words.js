582cc14b3d779289d849447e8c42e4d6cd8c9ddf
Michael McDonnell <Michael.McDonnell067@gmail.com>
2022-06-21 14:33:24 -0500

    completed the word set generator

--- src/Words.js
+++ src/Words.js
@@ -9,7 +9,15 @@ export const boardDefault = [
     ["", "", "", "", "",],
 ];
 
-const generateWordSet = async () => {
+export const generateWordSet = async () => {
     let wordSet;
     await fetch(wordBank)
+    .then((response) => response.text())
+    .then((result) =>{
+        const wordArr = result.split("\n")
+        wordSet = new Set(wordArr);
+    });
+
+    return { wordSet };
+
 }
